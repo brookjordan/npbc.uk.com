a1bf339a186b544e9aabf838048d0284e87968a7
Brook Jordan <brkjrdn@gmail.com>
2025-07-21 11:45:02 +0800

    init

---
+++ menumachine/npbc_a/menuspecs.js
@@ -0,0 +1,21 @@
+/*
+MenuMachine 2 definition file - do not edit. http://menumachine.com
+2.1.1 :: npbc-a
+*/
+var menuName="npbc_a";
+var pkg=new menuPackage(menuName,1,0,0,0,1,1,0,0,1);
+/*s*/
+pkg.aB("m8e5b63a","",6.875,14.0625,0,0,0,0,0,0,0,0,0,"v",100,"",0,0,"",0,"black",0,0,"black",0,0,0,"","","","",0,0,0,0,0,0,0,0,0);
+pkg.aI("m9dbb232","m8e5b63a",6.875,1.5625,0,0,"Home","",/*URL*/"../../index.html","","","","",0,0,0,0,"","",0,0,"Trebuchet MS,Geneva,Arial,Helvetica,SunSans-Regular,sans-serif","","#5375b1",0.6875,0,0,0,"black",0,1,0,0,0.3125,"",1,"","","#be8f0f",0.6875,0,0,0,"black",0,1,0,0,0.3125,"",1,"","","gray",0.6875,1,0,0,"black",0,1,0,0,0.3125,"");
+pkg.aI("m14xjdot","m8e5b63a",6.875,1.5625,0,1.5625,"Services","",/*URL*/"../../pages/services.html","","","","",0,0,0,0,"","",1,1);
+pkg.aI("m14xjdoc","m8e5b63a",6.875,1.5625,0,3.125,"Activities","",/*URL*/"../../pages/activities.html","","","","",0,0,0,0,"","",2,1);
+pkg.aI("m16l4fh4","m8e5b63a",6.875,1.5625,0,4.6875,"Newsletter","",/*URL*/"../../pages/newsletter.html","","","","",0,0,0,0,"","",3,1);
+pkg.aI("m16l4fh5","m8e5b63a",6.875,1.5625,0,6.25,"Sermons","",/*URL*/"../../pages/sermons.html","","","","",0,0,0,0,"","",4,1);
+pkg.aI("m16l4fh6","m8e5b63a",6.875,1.5625,0,7.8125,"5 minutes","",/*URL*/"../../pages/5-mins.html","","","","",0,0,0,0,"","",5,1);
+pkg.aI("m16l4fh7","m8e5b63a",6.875,1.5625,0,9.375,"Directions","",/*URL*/"../../pages/directions.html","","","","",0,0,0,0,"","",6,1);
+pkg.aI("m187yt10","m8e5b63a",6.875,1.5625,0,10.9375,"Contacts","",/*URL*/"../../pages/contacts.html","","","","",0,0,0,0,"","",7,1);
+pkg.aI("m187yt11","m8e5b63a",6.875,1.5625,0,12.5,"Links","",/*URL*/"../../pages/links.html","","","","",0,0,0,0,"","",8,1);
+/*f*/
+__menuHolder.aP(pkg);
+var __l=__br.un?"un.js":(__br.ie?"iedom.js":(__br.dom?"w3cdom.js":""));
+document.write(_sTs+mmfolder+"core/"+__l+_sTe);
